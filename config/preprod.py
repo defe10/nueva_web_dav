ef3884eff125fc53f743a1eef3546c76b883f795
defe10 <191590799+defe10@users.noreply.github.com>
2025-12-19 17:36:05 -0700

    fix preprod: forzar ALLOWED_HOSTS para pythonanywhere

--- config/preprod.py
+++ config/preprod.py
@@ -2,7 +2,8 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["defe10.pythonanywhere.com",]
+ALLOWED_HOSTS = ["defe10.pythonanywhere.com"]
+
 CSRF_TRUSTED_ORIGINS = [
     "https://defe10.pythonanywhere.com",
 ]
