6cbd04ed499173acf0fb6918f0664efbf1f215ba
defe10 <191590799+defe10@users.noreply.github.com>
2025-12-20 11:18:34 -0700

    correo electronico

--- config/preprod.py
+++ config/preprod.py
@@ -10,3 +10,20 @@ CSRF_TRUSTED_ORIGINS = [
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 USE_X_FORWARDED_HOST = True
+
+
+# ============================================
+# EMAIL
+# ============================================
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "federicocasoni@edusalta.gov.ar"
+EMAIL_HOST_PASSWORD = "qmyw qjxc iyve bpbr"
+
+DEFAULT_FROM_EMAIL = "Dirección de Audiovisuales <federicocasoni@edusalta.gov.ar>"
+
